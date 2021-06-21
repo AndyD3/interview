6d15167a15a39abd1e50b8f7b4e910823e3f999f
Andrew Douglas <andrew.douglas2@sky.uk>
2021-06-21 16:05:14 +0100

    formatting correction

--- src/test/java/com/douglas/andy/interview/controllers/DashboardControllerWebLayerTest.java
+++ src/test/java/com/douglas/andy/interview/controllers/DashboardControllerWebLayerTest.java
@@ -3,7 +3,6 @@ package com.douglas.andy.interview.controllers;
 import com.douglas.andy.interview.entities.Dashboard;
 import com.douglas.andy.interview.service.DashboardService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
